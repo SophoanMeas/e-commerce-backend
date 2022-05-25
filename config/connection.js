298cf2e361df3bac1c4afb7af90c884bad76c169
Ponny Meas <sophoan.meas@hotmail.com>
2022-05-24 22:22:04 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const sequelize = require('sequelize');
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
